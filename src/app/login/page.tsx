2df347b04eec4d2d8b8f838790c21a6428ca2d82
Ankit Kumar Singh <ankit.singh@fairview.org>
2024-08-13 12:33:03 +0530

    Login page Changes

---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import SignInForm from "@/app/component/auth/SignInForm";
+
+export default function Login() {
+  return (
+    <>
+      <SignInForm />
+    </>
+  );
+}
